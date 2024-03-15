432f187e4a16358cdb0588d8e64668a31aec6bbf
J. Rafael Torrealba <joser.campost@gmail.com>
2024-03-15 18:56:25 +0100

    `text_form_report()` Baseline with `beautifulsoup4`

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 HTM_PATH = r"data/htm/"
+TXT_PATH = r"data/text/"
 
 
 def get_sec_form(url_: str, chrome_options: Options() = None):
@@ -29,15 +31,49 @@ def get_sec_form(url_: str, chrome_options: Options() = None):
         EC.presence_of_element_located((By.ID, "fact-identifier-5"))
     )
 
-    save_path = HTM_PATH + url_.split("data/")[1].replace("/", "-")
+    htm_report_path = HTM_PATH + url_.split("data/")[1].replace("/", "-")
     # Save the page source to an HTML file
-    with open(save_path, "w", encoding="utf-8") as file:
+    with open(htm_report_path, "w", encoding="utf-8") as file:
         file.write(driver.page_source)
-        print(f"Stored at {save_path} successfully")
+        print(f"Stored at {htm_report_path} successfully")
 
-    return save_path
+    return htm_report_path
+
+
+def text_form_report(htm_report_path):
+    # Read the HTML file
+    with open(htm_report_path, "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # Remove script, style, and unwanted tags
+    unwanted_tags = ["script", "style", "header", "footer", "nav", "aside", "figure", "figcaption",
+                     "table", "thead", "tbody"]
+
+    for tag in unwanted_tags:
+        for element in soup.find_all(tag):
+            element.decompose()
+
+    # Remove unwanted attributes from tags
+    for tag in soup.find_all(True):
+        tag.attrs = {}
+
+    # Extract the clean text
+    clean_text = soup.get_text(separator="\n", strip=True)
+
+    str_report_path = TXT_PATH + htm_report_path.split("htm/")[1].split(".")[0] + ".txt"
+
+    # Write the clean text to a file
+    with open(str_report_path, "w", encoding="utf-8") as file:
+        file.write(clean_text)
+
+    print("Clean text extracted and saved to clean_text.txt")
+    return str_report_path
 
 
 if __name__ == "__main__":
-    url_ = "https://www.sec.gov/ixviewer/ix.html?doc=/Archives/edgar/data/101778/000010177823000146/mro-20230930.htm"
-    get_sec_form(url_)
+    # url_ = "https://www.sec.gov/ixviewer/ix.html?doc=/Archives/edgar/data/101778/000010177823000146/mro-20230930.htm"
+    # get_sec_form(url_)
+    text_form_report("data/htm/101778-000010177823000146-mro-20230930.htm")
